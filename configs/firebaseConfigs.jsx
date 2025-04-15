db96954debb820a60404706039e9b5d96b8c3dae
niteesh577 <95271997+niteesh577@users.noreply.github.com>
2025-04-15 12:03:45 +0530

    Update firebaseConfigs.jsx

--- configs/firebaseConfigs.jsx
+++ configs/firebaseConfigs.jsx
@@ -8,12 +8,12 @@ import {getAuth} from "firebase/auth"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "fact-buddy-74b37.firebaseapp.com",
-  projectId: "fact-buddy-74b37",
-  storageBucket: "fact-buddy-74b37.firebasestorage.app",
-  messagingSenderId: "504344262298",
-  appId: "1:504344262298:web:36d327df3f61f7e8231123",
-  measurementId: "G-0YXHXNM5YL"
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
